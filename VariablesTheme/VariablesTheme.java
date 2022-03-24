ad16fb5c25b4b63c0d2a0a318238bf618278fdfe
Savtsov <savtsovao@yandex.ru>
2022-03-24 19:55:31 +0300

    remove spase

--- VariablesTheme/VariablesTheme.java
+++ VariablesTheme/VariablesTheme.java
@@ -1,6 +1,7 @@
 public class VariablesTheme {
 	public static void main(String[] args) {
 		//
-		System.out.println("Hello World!");
+		String HW = "Hello World!";
+		System.out.println(HW);
 	}
 } 
