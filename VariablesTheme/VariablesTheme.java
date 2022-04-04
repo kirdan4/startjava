63936c0b5bb6b9e26a800cb0562caeab4a4c9ce9
Savtsov <savtsovao@yandex.ru>
2022-04-04 20:13:53 +0300

    V.03 Работа над ошибками д/з Переменные

--- VariablesTheme/VariablesTheme.java
+++ VariablesTheme/VariablesTheme.java
@@ -1,7 +1,6 @@
 public class VariablesTheme {
 	public static void main(String[] args) {
-
-		// Создание переменных и вывод их значений
+		//Создание переменных и вывод их значений
 		System.out.println("1. Создание переменных и вывод их значений");
 		char markPc = '';
 		boolean isTrue = true;
@@ -21,17 +20,17 @@ public class VariablesTheme {
 		System.out.println("Weight = " + weight);
 		System.out.print("\n");
 
-		// Расчет стоимости товара со скидкой
+		//Расчет стоимости товара со скидкой
 		System.out.println("2. Расчет стоимости товара со скидкой");
 		int priseProduct1 = 100;
 		int priseProduct2 = 200;
 		int amontDiscount = (priseProduct1 + priseProduct2) * 11 / 100;
-		int totalPriceOfDiscouted = (priseProduct1 + priseProduct2) - (priseProduct1 + priseProduct2) * 11 / 100;
+		int totalPriceOfDiscouted = (priseProduct1 + priseProduct2) - amontDiscount;
 		System.out.println ("Сумма скидки = "+ amontDiscount + " rub.");
 		System.out.println ("Общая стоимость товаров со скидкой = "+ totalPriceOfDiscouted+ " rub.");
 		System.out.print("\n");
 
-		// Вывод на консоль слова JAVA
+		//Вывод на консоль слова JAVA
 		System.out.println("3. Вывод на консоль слова JAVA");
 		System.out.println("   J    a   v     v  a ");
 		System.out.println("   J   a a   v   v  a a");
@@ -39,65 +38,64 @@ public class VariablesTheme {
 		System.out.println(" JJ  a     a   V  a     a");
 		System.out.print("\n");
 
-		// Отображение min и max значений числовых типов данных
+		//Отображение min и max значений числовых типов данных
 		System.out.println("4. Отображение min и max значений числовых типов данных");
-		byte maxValue_byte = 127;
-		short maxValue_short  = 32767;
-		int maxValue_int = 2147483647;
-		long maxValue_long = 9223372036854775807L;
-		float maxValue_float = 3.4e+038f;
-		double maxValue_double = 1.7976931348623157E+308;
-		System.out.println("Мах byte = " + maxValue_byte);
-		System.out.println("Мах short = " + maxValue_short);
-		System.out.println("Мах int = " + maxValue_int);
-		System.out.println("Мах long = " + maxValue_long);
+		byte byteMaxValue = 127;
+		short shortMaxValue = 32767;
+		int intMaxValue = 2147483647;
+		long longMaxValue = 9223372036854775807L;
+		System.out.println("Мах byte = " + byteMaxValue);
+		System.out.println("Мах short = " + shortMaxValue);
+		System.out.println("Мах int = " + intMaxValue);
+		System.out.println("Мах long = " + longMaxValue);
 		System.out.print("\n");
-		// Инкрементация, декрементация значений числовых типов данных
-		System.out.println(++maxValue_byte);
-		System.out.println(--maxValue_byte);
-		System.out.println(++maxValue_short);
-		System.out.println(--maxValue_short);
-		System.out.println(++maxValue_int);
-		System.out.println(--maxValue_int);
-		System.out.println(++maxValue_long);
-		System.out.println(--maxValue_long);
+		//Инкрементация, декрементация значений числовых типов данных
+		System.out.println(++byteMaxValue);
+		System.out.println(--byteMaxValue );
+		System.out.println(++shortMaxValue);
+		System.out.println(--shortMaxValue);
+		System.out.println(++intMaxValue);
+		System.out.println(--intMaxValue);
+		System.out.println(++longMaxValue);
+		System.out.println(--longMaxValue);
 		System.out.print("\n");
 
-		// Перестановка местами значений переменных (с дополнительной переменной) 
+		//Перестановка местами значений переменных (с дополнительной переменной) 
 		System.out.println("5. Перестановка местами значений переменных (с вводом дополнительной переменной");
-		float var1 = 2.1f;
-		float var2 = 3.6f;
-		float var3;
-		System.out.println("var1 = " + var1);
-		System.out.println("var2 = " + var2);
-		var3 = var1;
-		var1 = var2;
-		var2 = var3;
-		System.out.println("var1 = " + var1);
-		System.out.println("var2 = " + var2);
+		float floatValue1 = 2.1f;
+		float floatValue2 = 3.6f;
+		float addVariable;
+		System.out.println("var1 = " + floatValue1);
+		System.out.println("var2 = " + floatValue2);
+		addVariable = floatValue1;
+		floatValue1 = floatValue2;
+		floatValue2 = addVariable;
+		System.out.println("var1 = " + floatValue1);
+		System.out.println("var2 = " + floatValue2);
 		System.out.print("\n");
 
 		//Вывод символов и их кодов
 		System.out.println("6. Вывод символов и их кодов");
-		int variable1 = 35;
-		int variable2 = 38;
-		int variable3 = 64;
-		int variable4 = 94;
-		int variable5 = 95;
-		System.out.println(variable1 + " " + (char) variable1);
-		System.out.println(variable2 + " " + (char) variable2);
-		System.out.println(variable3 + " " + (char) variable3);
-		System.out.println(variable4 + " " + (char) variable4);
-		System.out.println(variable5 + " " + (char) variable5);
+		int symbol35 = 35;
+		int symbol38 = 38;
+		int symbol64 = 64;
+		int symbol94 = 94;
+		int symbol95 = 95;
+		System.out.println(symbol35 + " " + (char) symbol35);
+		System.out.println(symbol38 + " " + (char) symbol38);
+		System.out.println(symbol64 + " " + (char) symbol64);
+		System.out.println(symbol94 + " " + (char) symbol94);
+		System.out.println(symbol95 + " " + (char) symbol95);
 		System.out.print("\n");
 
 		//Произведение и сумма цифр числа (без цикла)
 		System.out.println("7. Произведение и сумма цифр числа 345 (без цикла)");
-		int num1 = 3;
-		int num2 = 4;
-		int num3 = 5;
-		System.out.println(num1+num2+num3);
-		System.out.println(num1*num2*num3);
+		int threeDigitNumber = 345;
+		int firstDigit = threeDigitNumber / 100;
+		int secondDigit = threeDigitNumber / 10 % 10;
+		int thirdDigit = threeDigitNumber % 10;
+		System.out.println(firstDigit + secondDigit + thirdDigit);
+		System.out.println(firstDigit * secondDigit * thirdDigit);
 		System.out.print("\n");
 
 		//Вывод на консоль ASCII-арт Дюка
@@ -124,22 +122,21 @@ public class VariablesTheme {
 
 		//Отображение количества сотен, десятков и единиц числа
 		System.out.println("9. Отображение количества сотен, десятков и единиц числа 123");
-		int a = 123;
-		int x1 = a/100;
-		int x2 = a%100/10;
-		int x3 = a%10;
-		System.out.println("Кол-во сотен = " + x1);
-		System.out.println("Кол-во десятков = " + x2);
-		System.out.println("Кол-во единиц = " + x3);
+		int srcNum = 123;
+		int hundreds = srcNum / 100;
+		int dozens = srcNum % 100 / 10;
+		int units = srcNum % 10;
+		System.out.println("Кол-во сотен = " + hundreds);
+		System.out.println("Кол-во десятков = " + dozens);
+		System.out.println("Кол-во единиц = " + units);
 		System.out.print("\n");
 
-		// Преобразование секунд
+		//Преобразование секунд
 		System.out.println("10. Преобразование секунд в часы, минуты, секунды");
 		int time = 86399;
-		int sec = time%60;
-		int hour = time/60;
-		int min = hour%60;
-		hour = hour / 60;
-		System.out.println(hour + ":" + min + ":" + sec);
+		int hour = time / 60 / 60;
+		int min = time / 60 % 60;
+		int sec = time % 60;
+				System.out.println(hour + ":" + min + ":" + sec);
 	}
 }
