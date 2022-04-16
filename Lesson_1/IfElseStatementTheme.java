bfb8d8e25d1b38394dc28cdc917879cac4464829
Savtsov <savtsovao@yandex.ru>
2022-04-16 11:54:39 +0300

    Новая структура ДЗ

---
+++ Lesson_1/IfElseStatementTheme.java
@@ -0,0 +1,210 @@
+public class IfElseStatementTheme {
+	public static void main(String[] args) {
+		//Перевод псевдокода на язык Java
+		System.out.println("1. Перевод псевдокода на язык Java");
+		int age = 32;
+		if (age > 20) {
+			System.out.println("Тебе больше 20!");
+		} else {
+			System.out.println("Тебе меньше 20!");
+		}
+		String sex = "women";
+		if (sex != "man") {
+			System.out.println("Ты не мужик!");
+		}
+		float height = 1.7f;
+		if (height < 1.8f) {
+			System.out.println("Ты коротышка!");
+		} else {
+			System.out.println("Ты колонча!");
+		}
+		String name =  "Alex";
+		char symbol1Name = name.charAt (0);
+		if (symbol1Name == 'M') {
+			System.out.println("Это хорошо!");
+		} else if (symbol1Name == 'I') {
+			System.out.println("Это очень хорошо!");
+		} else {
+			System.out.println("Проходи!");
+		}
+
+		//Поиск максимального и минимального числа
+		System.out.println("\n2. Поиск максимального и минимального числа");
+		int num1 = 12;
+		int num2 = 7;
+		if (num1 > num2) {
+			System.out.printf("Между %d и %d, максимальное число %d %n", num1, num2, num1);
+		} else {
+			System.out.printf("Между %d и %d, максимальное число %d %n", num1, num2, num2);
+		}
+		if (num1 < num2) {
+			System.out.printf("Между %d и %d, минимальное число %d %n", num1, num2, num1);
+		} else {
+			System.out.printf("Между %d и %d, минимальное число %d %n", num1, num2, num2);
+		}
+
+		//Работа с числом
+		System.out.println ("\n3. Работа с числом");
+		int num = 15;
+		if (num % 2 == 0) {
+			System.out.printf ("Число %d четное %n", num);
+		} else {
+			System.out.printf ("Число %d нечетное %n", num);
+		}
+		if (num < 0) {
+			System.out.printf ("Число %d отрицательное %n", num);
+		} else {
+			System.out.printf ("Число %d положительное %n", num);
+		}
+		if (num == 0) {
+			System.out.println ("Число ноль");
+		} else {
+			System.out.println ("Число не ноль");
+		}
+
+		//Поиск общей цифры в числах
+		System.out.println("\n4. Поиск общей цифры в числах");
+		int srcNum1 = 561;
+		int srcNum2 = 871;
+		if (srcNum1 / 100 == srcNum2 / 100) {
+			System.out.println("Однинаковая цыфра в сотнях = " + srcNum1 / 100);
+		}
+		if (srcNum1 % 100 / 10 == srcNum2 % 100 / 10) {
+			System.out.println("Однинаковая цыфра в десятках = " + srcNum1 % 100 / 10);
+		}
+		if (srcNum1 % 10 == srcNum2 % 10) {
+			System.out.println("Однинаковая цыфра в единицах = " + srcNum1 % 10);
+		}
+
+		//Определение буквы, числа или символа по их коду
+		System.out.println("\n5. Определение буквы, числа или символа по их коду");
+		char sumbolZ = '\u005A';
+		if (sumbolZ > 64 && sumbolZ < 91) {
+			System.out.println(sumbolZ + " - заглавная буква");
+		} else if (sumbolZ > 96 && sumbolZ < 123) {
+			System.out.println(sumbolZ + " - строчная буква");
+		} else if (sumbolZ > 47 && sumbolZ < 58) {
+			System.out.println(sumbolZ + " - число");
+		} else {
+			System.out.println(sumbolZ + " - символ");
+		}
+
+		//Определение суммы вклада и начисленных банком %
+		System.out.println("\n6. Определение суммы вклада и начисленных банком %");
+		float depositAmount = 300000;
+		float accruedInterest = 0;
+		if (depositAmount < 100000) {
+			accruedInterest = depositAmount * 0.05f;
+		} else if (depositAmount > 100000 && depositAmount <= 300000) {
+			accruedInterest = depositAmount * 0.07f;
+		} else if (depositAmount > 300000) {
+			accruedInterest = depositAmount * 0.1f;
+		}
+		System.out.println("Cумму вклада = " + depositAmount);
+		System.out.println("Начисленный % = " + accruedInterest);
+		System.out.println("Итоговая сумма = " + (depositAmount + accruedInterest));
+
+		//Определение оценки по предметам
+		System.out.println("\n7. Определение оценки по предметам");
+		int historyPercent = 59;
+		int compSciencePercent = 91;
+		int historyScore = 0;
+		int compScienceScore = 0;
+		if (historyPercent <= 60) {
+			historyScore = 2;
+		} else if (historyPercent > 60 && historyPercent <= 73) {
+			historyScore = 3;
+		} else if (historyPercent > 73 && historyPercent <= 91) {
+			historyScore = 4;
+		} else if (historyPercent > 91) {
+			historyScore = 5;
+		}
+		if (compSciencePercent <= 60) {
+			compScienceScore = 2;
+		} else if (compSciencePercent > 60 && compSciencePercent <= 73) {
+			compScienceScore = 3;
+		} else if (compSciencePercent > 73 && compSciencePercent <= 91) {
+			compScienceScore = 4;
+		} else if (compSciencePercent > 91) {
+			compScienceScore = 5;
+		}
+		System.out.println(historyScore + " - История");
+		System.out.println(compScienceScore + " - Информатика");
+		System.out.println((historyScore + compScienceScore) / 2 + " - Средний бал");
+		System.out.println((historyPercent + compSciencePercent) / 2 + " - Средний % по предметам");
+
+		//Расчет прибыли (убытка)
+		System.out.println("\n8. Расчет прибыли (убытка)");
+		int costPrice = 9000;
+		int rent = 5000;
+		int revenue = 15000;
+		int profitYear = (revenue - costPrice - rent) * 12;
+		if (profitYear > 0) {
+			System.out.println("Прибыль(+)/Убыток(-) за год = +" + profitYear);
+		} else if (profitYear < 0) {
+			System.out.println("Прибыль(+)/Убыток(-) за год = " + profitYear);
+		} else {
+			System.out.println("Прибыль(+)/Убыток(-) за год = " + 0);
+		}
+
+		//Определение существования треугольника
+		System.out.println("\n9. Определение существования треугольника");
+		double side1 = 4;
+		double side2 = 5;
+		double side3 = 3;
+		if (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1) {
+			System.out.println("Треугольник существует.");
+			if (side3 == Math.sqrt (side1 * side1 + side2 * side2)) {
+				System.out.println ("Сторона 3 - гипотенуза \nСторона 1 и Сторона 2 - катеты");
+			}
+			if (side1 == Math.sqrt (side2 * side2 + side3 * side3)) {
+				System.out.println ("Сторона 1 - гипотенуза \nСторона 2 и Сторона 3 - катеты");
+			}
+			if (side2 == Math.sqrt (side1 * side1 + side3 * side3)) {
+				System.out.println ("Сторона 2 - гипотенуза \nСторона 1 и Сторона 3 - катеты");
+			}
+			double pP = (side1 + side2 + side3) / 2;
+			double area = Math.sqrt (pP * (pP - side1) * (pP - side2) * (pP - side3));
+			System.out.println("Площадь = " + area);
+/*
+|\    
+| \   
+|  \  
+|___\ 
+*/
+			char pipe = 124;
+			char bsl = 92;
+			char uLine = 95;
+			char space = 32;
+			System.out.println("" + pipe + bsl);
+			System.out.println("" + pipe + space + bsl);
+			System.out.println("" + pipe + space + space + bsl);
+			System.out.println("" + pipe + uLine + uLine + uLine + bsl);
+		} else {
+			System.out.println("Треугольник не существует.");
+		}
+
+		//Определение существования треугольника
+		System.out.println("\n10. Подсчет количества банкнот");
+		int sum = 567;
+		int bank50 = 0;
+		int bank10 = 0;
+		int bank1 = 0;
+		if (sum >= 50) {
+			bank50 = sum / 50;
+			sum = sum - (bank50 * 50);
+		}
+		if (sum < 50 && sum >= 9) {
+			bank10 = sum / 10;
+			sum = sum - (bank10 * 10);
+		}
+		if (sum <= 9 && sum > 0) {
+			bank1 = sum / 1;
+			sum = sum - (bank1 * 1);
+		}
+		System.out.println("Номиналы банкнот = 50, 10, 1");
+		System.out.println("Сумма = " + ((bank50 * 50) + (bank10 * 10) + (bank1 *1)));
+		System.out.println("Необходимо банкнот = " + (bank50 + bank10 + bank1));
+		System.out.printf("Номиналом 50 - %d шт.%nНоминалом 10 - %d шт. %nНоминалом 1 - %d шт.%n", bank50, bank10, bank1);
+	}
+}
---
+++ Lesson_1/VariablesTheme.java
@@ -0,0 +1,133 @@
+public class VariablesTheme {
+	public static void main(String[] args) {
+		//Создание переменных и вывод их значений
+		System.out.println("1. Создание переменных и вывод их значений");
+		char markPc = '';
+		boolean isTrue = true;
+		byte ram = 8;
+		short hdd = 128;
+		int vRam = 1536;
+		long cpu = 2700;
+		float diagonal = 13.3f;
+		double weight = 1.58d;
+		System.out.println("marc PC = " + markPc);
+		System.out.println(isTrue);
+		System.out.println("RAM = " + ram);
+		System.out.println("HDD = " + hdd);
+		System.out.println("VRAM = " + vRam);
+		System.out.println("CPU = " + cpu);
+		System.out.println("Diagonal = " + diagonal);
+		System.out.println("Weight = " + weight);
+
+		//Расчет стоимости товара со скидкой
+		System.out.println("\n2. Расчет стоимости товара со скидкой");
+		int priseProduct1 = 100;
+		int priseProduct2 = 200;
+		int amontDiscount = (priseProduct1 + priseProduct2) * 11 / 100;
+		int totalPriceOfDiscouted = (priseProduct1 + priseProduct2) - amontDiscount;
+		System.out.println ("Сумма скидки = "+ amontDiscount + " rub.");
+		System.out.println ("Общая стоимость товаров со скидкой = "+ totalPriceOfDiscouted+ " rub.");
+
+		//Вывод на консоль слова JAVA
+		System.out.println("\n3. Вывод на консоль слова JAVA");
+		System.out.println("   J    a   v     v  a ");
+		System.out.println("   J   a a   v   v  a a");
+		System.out.println("J  J  aaaaa   V V  aaaaa");
+		System.out.println(" JJ  a     a   V  a     a");
+
+		//Отображение min и max значений числовых типов данных
+		System.out.println("\n4. Отображение min и max значений числовых типов данных");
+		byte byteMaxValue = 127;
+		short shortMaxValue = 32767;
+		int intMaxValue = 2147483647;
+		long longMaxValue = 9223372036854775807L;
+		System.out.println("Мах byte = " + byteMaxValue);
+		System.out.println("Мах short = " + shortMaxValue);
+		System.out.println("Мах int = " + intMaxValue);
+		System.out.println("Мах long = " + longMaxValue);
+
+		//Инкрементация, декрементация значений числовых типов данных
+		System.out.println("\n" + ++byteMaxValue);
+		System.out.println(--byteMaxValue );
+		System.out.println(++shortMaxValue);
+		System.out.println(--shortMaxValue);
+		System.out.println(++intMaxValue);
+		System.out.println(--intMaxValue);
+		System.out.println(++longMaxValue);
+		System.out.println(--longMaxValue);
+
+		//Перестановка местами значений переменных (с дополнительной переменной) 
+		System.out.println("\n5. Перестановка местами значений переменных (с вводом дополнительной переменной");
+		float value1 = 2.1f;
+		float value2 = 3.6f;
+		float addVariable;
+		System.out.println("var1 = " + value1);
+		System.out.println("var2 = " + value2);
+		addVariable = value1;
+		value1 = value2;
+		value2 = addVariable;
+		System.out.println("var1 = " + value1);
+		System.out.println("var2 = " + value2);
+
+		//Вывод символов и их кодов
+		System.out.println("\n6. Вывод символов и их кодов");
+		int symbol35 = 35;
+		int symbol38 = 38;
+		int symbol64 = 64;
+		int symbol94 = 94;
+		int symbol95 = 95;
+		System.out.println(symbol35 + " " + (char) symbol35);
+		System.out.println(symbol38 + " " + (char) symbol38);
+		System.out.println(symbol64 + " " + (char) symbol64);
+		System.out.println(symbol94 + " " + (char) symbol94);
+		System.out.println(symbol95 + " " + (char) symbol95);
+
+		//Произведение и сумма цифр числа (без цикла)
+		System.out.println("\n7. Произведение и сумма цифр числа 345 (без цикла)");
+		int srcNumber= 345;
+		int firstDigit = srcNumber / 100;
+		int secondDigit = srcNumber / 10 % 10;
+		int thirdDigit = srcNumber % 10;
+		System.out.println(firstDigit + secondDigit + thirdDigit);
+		System.out.println(firstDigit * secondDigit * thirdDigit);
+		
+		//Вывод на консоль ASCII-арт Дюка
+		/*
+    /\    
+   /  \   
+  /_( )\  
+ /      \ 
+/____/\__\
+*/
+		System.out.println("\n8. Вывод на консоль ASCII-арт Дюка");
+		char sp = ' ';
+		char sl = 47;
+		char bsl = 92;
+		char uLine = 95;
+		char brac1 = '(';
+		char brac2 = ')';
+		System.out.println("" + sp + sp + sp + sp + sl + bsl + sp + sp + sp + sp);
+		System.out.println("" + sp + sp + sp + sl + sp + sp + bsl + sp + sp + sp);
+		System.out.println("" + sp + sp + sl + uLine + brac1 + sp + brac2 + bsl + sp + sp);
+		System.out.println("" + sp + sl + sp + sp + sp + sp + sp + sp + bsl + sp);
+		System.out.println("" + sl + uLine + uLine + uLine + uLine + sl + bsl + uLine + uLine + bsl);
+
+		//Отображение количества сотен, десятков и единиц числа
+		System.out.println("\n9. Отображение количества сотен, десятков и единиц числа 123");
+		int srcNum = 123;
+		int hundreds = srcNum / 100;
+		int dozens = srcNum % 100 / 10;
+		int units = srcNum % 10;
+		System.out.println("Кол-во сотен = " + hundreds);
+		System.out.println("Кол-во десятков = " + dozens);
+		System.out.println("Кол-во единиц = " + units);
+
+		//Преобразование секунд
+		System.out.println("\n10. Преобразование секунд в часы, минуты, секунды");
+		int time = 86399;
+		int hour = time / 60 / 60;
+		int min = time / 60 % 60;
+		int sec = time % 60;
+		System.out.println(hour + ":" + min + ":" + sec);
+	}
+}
