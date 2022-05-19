ddf398476679878c97a43d41dc7458a05d4f5fe4
Savtsov <savtsovao@yandex.ru>
2022-05-19 19:56:57 +0300

    ДЗ Класс Person v.1.1(убрал лишние строки)

--- Lesson_2/Person.java
+++ Lesson_2/Person.java
@@ -1,11 +1,5 @@
 public class Person {
 
-// 		Создайте класс Person (без метода main)
-// 		Напишите в нем поля (атрибуты), описывающие характеристики человека: 
-	// пол, имя, рост, вес, возраст
-// 		Проинициализируйте их какими-то значениями
-// 		Создайте в классе методы, описывающие поведение (что он может делать)
-	// человека: идти, сидеть, бежать, говорить, учить Java
 	String name = "Alex";
 	String sex = "men";
 	int age  = 18;
@@ -13,26 +7,26 @@ public class Person {
 	float weight = 180;
 
 	public void go() {
-		System.out.println("Man goes");
+		System.out.println("Person goes");
 	}
 
 	public void run() {
-		System.out.println("Man running");
+		System.out.println("Person running");
 	}
 
 	public void sit() {
-		System.out.println("Man sat");
+		System.out.println("Person sat");
 	}
 
 	public void jump() {
-		System.out.println("Man jumped");
+		System.out.println("Person jumped");
 	}
 
 	public void sayHello() {
-		System.out.println("Man said Hello");
+		System.out.println("Person said Hello");
 	}
 
 	public void learnJava() {
-		System.out.println("Man learning java");
+		System.out.println("Person learning java");
 	}
 }
