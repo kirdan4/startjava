6b965b24808edc32d73cf61828c4f9064ff575f6
Savtsov <savtsovao@yandex.ru>
2022-07-28 19:41:43 +0300

    Работа над ошибками Lesson_4 ДЗ Массивы. Цикл foreach. v.1.3.

--- src/com/startjava/lesson_4/ArrayTheme.java
+++ src/com/startjava/lesson_4/ArrayTheme.java
@@ -3,52 +3,47 @@ package com.startjava.lesson_4;
 import java.util.Arrays;
 
 public class ArrayTheme {
-    static void printArray(int[] array){
-        for(int i : array) {
-            System.out.print(i + " ");
-        }
-    }
 
     public static void main(String[] args) {
         System.out.println("1. Реверс значений массива");
         int[] intArray = {1, 2, 3, 4, 5, 6, 7};
-        int lenthArray = intArray.length;
+        int length = intArray.length;
         printArray(intArray);
-        System.out.println();
+
         //reverse array
-        for (int i = 0; i < lenthArray / 2; i++) {
-            int temp = intArray[lenthArray - i - 1];
-            intArray[lenthArray - i - 1] = intArray[i];
+        for (int i = 0; i < length / 2; i++) {
+            int temp = intArray[length - i - 1];
+            intArray[length - i - 1] = intArray[i];
             intArray[i] = temp;
         }
         printArray(intArray);
 
-        System.out.println("\n\n2. Вывод произведения элементов массива");
+        System.out.println("\n2. Вывод произведения элементов массива");
         intArray = new int[10];
-        lenthArray = intArray.length;
-        for (int i = 0; i < lenthArray; i++) {
+        length = intArray.length;
+        for (int i = 0; i < length; i++) {
             intArray[i] = i;
         }
         int prodNumbers = 1;
-        for (int i = 1; i < lenthArray - 1; i++) {
+        for (int i = 1; i < length - 1; i++) {
             prodNumbers *= intArray[i];
-            System.out.print((i < lenthArray - 2) ? intArray[i] + " * " : (intArray[i] + " = " + prodNumbers) + "\n");
+            System.out.print(intArray[i] + ((i < length - 2) ? " * " :  " = " + prodNumbers));
         }
-        System.out.printf("Под индексом '0' в массиве значение %d. " +
+        System.out.printf("\nПод индексом '0' в массиве значение %d. " +
                 "\nПод индексом '9' в массиве значение %d. \n", intArray[0], intArray[9]);
 
         System.out.println("\n3. Удаление элементов массива");
         float[] floatArray = new float[15];
-        lenthArray = floatArray.length;
+        length = floatArray.length;
         int countZeroCell = 0;
         //filling array with random unique values [0-1)
-        for (int i = 0; i < lenthArray; i++) {
+        for (int i = 0; i < length; i++) {
             System.out.print((floatArray[i] = (float) Math.random()) + " ");
         }
-        System.out.println("\nNumber from middle cell = " + floatArray[lenthArray / 2]);
+        System.out.println("\nNumber from middle cell = " + floatArray[length / 2]);
         //rewriting all numbers that are greater than the value of the middle cell of the array with the number 0
-        for (int i = 0; i < lenthArray; i++) {
-            if (floatArray[i] > floatArray[lenthArray / 2]) {
+        for (int i = 0; i < length; i++) {
+            if (floatArray[i] > floatArray[length / 2]) {
                 floatArray[i] = 0;
                 countZeroCell++;
             }
@@ -58,13 +53,13 @@ public class ArrayTheme {
 
         System.out.print("\n4. Вывод элементов массива лесенкой в обратном порядке");
         char[] charArray = new char[26];
-        lenthArray = charArray.length;
+        length = charArray.length;
         //creating an array of characters (A -Z )
-        for (int i = 0; i < lenthArray; i++) {
+        for (int i = 0; i < length; i++) {
             charArray[i] = (char) (65 + i);
         }
         //displaying an array in reverse order ladder
-        for (int i = 0; i <= lenthArray; i++) {
+        for (int i = 0; i <= length; i++) {
             for (int j = 0; j < i; j++) {
                 System.out.print(charArray[25 - j]);
             }
@@ -73,24 +68,23 @@ public class ArrayTheme {
 
         System.out.println("\n5. Генерация уникальных чисел");
         intArray = new int[30];
-        lenthArray = intArray.length;
+        length = intArray.length;
         int j;
         //filling array with random unique values [60-100]
-        for (int i = 0; i < lenthArray; i++) {
+        for (int i = 0; i < length; i++) {
             int newRandomValue = (int) (60 + Math.random() * 41);
             j = 0;
             do {
-                if (newRandomValue != intArray[j]) {
-                    intArray[i] = newRandomValue;
-                } else {
+                if (newRandomValue == intArray[j]) {
                     i--;
                     break;
                 }
+                intArray[i] = newRandomValue;
                 j++;
             } while (j < i);
         }
 //        bubble sorter
-        for (int i = lenthArray - 1; i > 0; i--) {
+        for (int i = length - 1; i > 0; i--) {
             for (j = 0; j < i; j++) {
                 if (intArray[j] > intArray[j + 1]) {
                     int temp = intArray[j];
@@ -100,41 +94,44 @@ public class ArrayTheme {
             }
         }
         //displaying an array of 10 numbers per line
-        for (int i = 0; i < lenthArray; i++) {
-            if (i % 10 == 0 && i > 0) {
-                System.out.println();
-            }
-            System.out.print(intArray[i] + " ");
+        for (int i = 1; i < length + 1; i++) {
+            System.out.printf("%d%s", intArray[i - 1], (i % 10 == 0) ? "\n" : " ");
         }
 
-        System.out.println("\n\n6. Сдвиг элементов массива");
+        System.out.println("\n6. Сдвиг элементов массива");
         String[] stringArray = {"", "AA", "", "", "BBB", "C", "", "DDDD"};
-        lenthArray = stringArray.length;
+        length = stringArray.length;
         int countLine = 0;
         //count the number of non-empty line
         for (String s : stringArray) {
-            if (!s.isEmpty()) {
+            if (!s.isBlank()) {
                 countLine++;
             }
         }
         //initialize a new array to the number of non-empty cells
-        String[] sortedStringArray = new String[countLine];
-        int lengthSortedStringArray = sortedStringArray.length;
+        String[] stringArrayNoBlankLines = new String[countLine];
+        int numberRowsCopy = stringArrayNoBlankLines.length;
         countLine = 0;
-        for (int i = 0; i < sortedStringArray.length; i++) {
-            for (j = 0; j < lenthArray; j++) {
+        for (int i = 0; i < stringArrayNoBlankLines.length; i++) {
+            for (j = 0; j < length; j++) {
                 j = countLine;
-                if (!stringArray[j].isEmpty()) {
-                    System.arraycopy(stringArray, j, sortedStringArray, i, lengthSortedStringArray);
-                    lengthSortedStringArray = lengthSortedStringArray - 1;
+                if (!stringArray[j].isBlank()) {
+                    System.arraycopy(stringArray, j, stringArrayNoBlankLines, i, numberRowsCopy);
+                    numberRowsCopy -= 1;
                     countLine++;
                     break;
-                } else {
-                    countLine++;
                 }
+                    countLine++;
             }
         }
         System.out.println(Arrays.toString(stringArray));
-        System.out.println(Arrays.toString(sortedStringArray));
+        System.out.println(Arrays.toString(stringArrayNoBlankLines));
+    }
+
+    private static void printArray(int[] nums) {
+        for(int num : nums) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
     }
 }
