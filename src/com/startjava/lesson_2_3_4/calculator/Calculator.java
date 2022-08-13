3cc291bfc5134fc53da04fe7a5a0eacc35e1a3e8
Savtsov <savtsovao@yandex.ru>
2022-08-13 14:46:36 +0300

    ДЗ Итоговое для Lesson_4 Calculator задания со * (пункты f, g, j)  v.2.0.

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -2,28 +2,23 @@ package com.startjava.lesson_2_3_4.calculator;
 
 public class Calculator {
 
-    public int calc(String mathExpression) {
-        String[] mathTerms = mathExpression.split("(?<=\\d)(?=\\D)|(?<=\\D)(?=\\d)");
-        int num1 = Integer.parseInt(mathTerms[0]);
-        int num2 = Integer.parseInt(mathTerms[2]);
+    public static int calc(String mathExpression) {
+        String[] operands = mathExpression.split("(?<=\\d)(?=\\D)|(?<=\\D)(?=\\d)");
+        int num1 = Integer.parseInt(operands[0]);
+        int num2 = Integer.parseInt(operands[2]);
         String tempOperation = mathExpression.replaceAll("[0-9]", "");
         char operation = tempOperation.charAt(0);
-        switch (operation) {
-            case '+':
-                return Math.addExact(num1, num2);
-            case '-':
-                return Math.subtractExact(num1, num2);
-            case '*':
-                return Math.multiplyExact(num1, num2);
-            case '/':
-                return Math.floorDiv(num1, num2);
-            case '^':
-                return (int) Math.pow(num1, num2);
-            case '%':
-                return Math.floorMod(num1, num2);
-            default:
-                System.out.println("Введён неправильный знак математической операции");
-        }
+        System.out.println( "Результат операции: " +
+            switch (operation) {
+                case '+' -> Math.addExact(num1, num2);
+                case '-' -> Math.subtractExact(num1, num2);
+                case '*' -> Math.multiplyExact(num1, num2);
+                case '/' -> Math.floorDiv(num1, num2);
+                case '^' -> (int) Math.pow(num1, num2);
+                case '%' -> Math.floorMod(num1, num2);
+                default -> "Введён неправильный знак математической операции";
+            }
+        );
         return 0;
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -6,15 +6,18 @@ public class CalculatorTest {
     public static void main(String[] args) {
         String reply = "yes";
         Scanner scan = new Scanner(System.in);
-        Calculator calculator = new Calculator();
         do {
             if ("yes".equals(reply)) {
                 System.out.println("Введите математическое выражение, без пробелов " +
                         "(число 1, знак математической операции (+, -, *, /, ^, %), число 2):");
-                System.out.println("Результат операции: " + calculator.calc(scan.next()));
+                try {
+                    Calculator.calc(scan.next());
+                } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+                    System.out.println("Вы ввели неправильное математическое выражение!");
+                }
             }
             System.out.println("Хотите продолжить вычисления? [yes/no]");
             reply = scan.next();
-        } while ("!no".equals(reply));
+        } while (!"no".equals(reply));
     }
 }
