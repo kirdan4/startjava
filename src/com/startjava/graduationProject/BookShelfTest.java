caa8080d700627ce7bdef7f89a202a1d14ee933f
Savtsov <savtsovao@yandex.ru>
2022-12-03 16:55:36 +0300

    BookShelf made static. Выпускной проект по StartJava "Книжный шкаф" v.1.2.

--- src/com/startjava/graduationProject/BookShelfTest.java
+++ src/com/startjava/graduationProject/BookShelfTest.java
@@ -3,15 +3,17 @@ package com.startjava.graduationProject;
 import java.util.Scanner;
 
 public class BookShelfTest {
+
+    private static final BookShelf BOOK_SHELF = new BookShelf();
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        BookShelf bookShelf = new BookShelf();
         String reply;
         do {
-            printBookShelf(bookShelf);
+            printBookShelf();
             printMenu();
             reply = scan.nextLine();
-            menu(bookShelf, scan, reply);
+            menu(scan, reply);
             String reply2;
             do {
                 System.out.println("Для продолжения работы нажмите Enter.");
@@ -32,24 +34,26 @@ public class BookShelfTest {
                 7. Выйти.""");
     }
 
-    private static void menu(BookShelf bookShelf, Scanner scan, String reply) {
+    private static void menu(Scanner scan, String reply) {
         switch (reply) {
-            case "1" -> addBook(bookShelf, scan);
-            case "2" -> findBook(bookShelf, scan);
-            case "3" -> deleteBook(bookShelf, scan);
-            case "4" -> printNumberOfBooks(bookShelf);
-            case "5" -> printFreeSpaceOnBookshelf(bookShelf);
-            case "6" -> bookShelf.cleanBookShelf();
+            case "1" -> addBook(scan);
+            case "2" -> findBook(scan);
+            case "3" -> deleteBook(scan);
+            case "4" -> printNumberOfBooks();
+            case "5" -> printFreeSpaceOnBookshelf();
+            case "6" -> BookShelfTest.BOOK_SHELF.cleanBookShelf();
             case "7" -> System.out.println("До свидания.");
             default -> System.out.println("Введено некорректное значение!");
         }
     }
 
-    private static void printBookShelf(BookShelf bookShelf) {
-        System.out.printf("В шкафу %d книги, свободно %d полок.\n", bookShelf.getNumberOfBooks(), bookShelf.getFreeSpaceOnBookshelf());
-        Book[] books = bookShelf.getBookShelf();
+    private static void printBookShelf() {
+        System.out.printf("В шкафу %d книги, свободно %d полок.\n",
+                BookShelfTest.BOOK_SHELF.getNumberOfBooks(),
+                BookShelfTest.BOOK_SHELF.getFreeSpaceOnBookshelf());
+        Book[] books = BookShelfTest.BOOK_SHELF.getBookShelf();
         for (Book book : books) {
-            if (bookShelf.getNumberOfBooks() == 0) {
+            if (BookShelfTest.BOOK_SHELF.getNumberOfBooks() == 0) {
                 System.out.println("Шкаф пуст. Вы можете добавить в него первую книгу.");
                 break;
             } else if (book != null) {
@@ -63,13 +67,15 @@ public class BookShelfTest {
         }
     }
 
-    private static void addBook(BookShelf bookShelf, Scanner scan) {
-        if (bookShelf.isFreeSpace()) {
+    private static void addBook(Scanner scan) {
+        if (BookShelfTest.BOOK_SHELF.isFreeSpace()) {
             System.out.println("Введите название книги, автора и год издания книги (через Enter):");
             try {
-                bookShelf.addBook(new Book(scan.nextLine(), scan.nextLine(), (Integer.parseInt(scan.nextLine()))));
+                BookShelfTest.BOOK_SHELF.addBook(new Book(scan.nextLine(), scan.nextLine(),
+                        (Integer.parseInt(scan.nextLine()))));
             } catch (NumberFormatException e) {
-                System.out.println("Книга не добавлена.\nВведенное значение (год издания книги) не является числом.");
+                System.out.println("Книга не добавлена.\nВведенное значение (год издания книги) " +
+                        "не является числом.");
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
@@ -78,35 +84,36 @@ public class BookShelfTest {
         }
     }
 
-    private static void findBook(BookShelf bookShelf, Scanner scan) {
+    private static void findBook(Scanner scan) {
         System.out.println("Введите название книги:");
         String searchedBook = scan.nextLine();
-        if (bookShelf.foundBook(searchedBook) == null) {
+        if (BookShelfTest.BOOK_SHELF.foundBook(searchedBook) == null) {
             System.out.println("Книги с таким названием нет на полке.");
         } else {
             System.out.print("----------------------------------\n");
-            System.out.print("|" + bookShelf.foundBook(searchedBook) + "      |\n");
+            System.out.print("|" + BookShelfTest.BOOK_SHELF.foundBook(searchedBook) + "      |\n");
             System.out.print("----------------------------------\n");
         }
     }
 
-    private static void deleteBook(BookShelf bookShelf, Scanner scan) {
+    private static void deleteBook(Scanner scan) {
         System.out.println("Введите название книги которую хотите удалить:");
         String searchedBook = scan.nextLine();
-        if (bookShelf.foundBook(searchedBook) == null) {
+        if (BookShelfTest.BOOK_SHELF.foundBook(searchedBook) == null) {
             System.out.println("Книги с таким названием нет на полке.");
         } else {
-            bookShelf.deleteBook(searchedBook);
+            BookShelfTest.BOOK_SHELF.deleteBook(searchedBook);
             System.out.println("Книга успешно удалена.");
         }
     }
 
-    private static void printNumberOfBooks(BookShelf bookShelf) {
-        System.out.printf("На полке %d книг.\n", bookShelf.getNumberOfBooks());
+    private static void printNumberOfBooks() {
+        System.out.printf("На полке %d книг.\n", BookShelfTest.BOOK_SHELF.getNumberOfBooks());
     }
 
-    private static void printFreeSpaceOnBookshelf(BookShelf bookShelf) {
-        System.out.printf("На полке %d свободных мест.\n", bookShelf.getFreeSpaceOnBookshelf());
+    private static void printFreeSpaceOnBookshelf() {
+        System.out.printf("На полке %d свободных мест.\n",
+                BookShelfTest.BOOK_SHELF.getFreeSpaceOnBookshelf());
     }
 }
 
